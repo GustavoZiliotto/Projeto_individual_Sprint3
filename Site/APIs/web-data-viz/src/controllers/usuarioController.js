60c11fb9c343056eda40a81d2b516276ed39454c
GustavoZiliotto <gustavo.bello@sptech.school>
2025-05-07 22:28:13 -0300

    Implementacao_parcial_API_e_comeco_dashboard

--- Site/APIs/web-data-viz/src/controllers/usuarioController.js
+++ Site/APIs/web-data-viz/src/controllers/usuarioController.js
@@ -2,16 +2,17 @@ var usuarioModel = require("../models/usuarioModel");
 var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
-    var email = req.body.emailServer;
+    var codigo = req.body.codigoServer;
     var senha = req.body.senhaServer;
+    var cpf = req.body.cpfServer;
 
-    if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+    if (codigo == undefined) {
+        res.status(400).send("Seu codigo está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está indefinida!");
-    } else {
-
-        usuarioModel.autenticar(email, senha)
+    } else if (cpf == undefined) {
+        res.status(400).send("Seu CPF está indefinida!");
+        usuarioModel.autenticar(codigo, senha, cpf)
             .then(
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
@@ -25,7 +26,7 @@ function autenticar(req, res) {
                                 if (resultadoAquarios.length > 0) {
                                     res.json({
                                         id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
+                                        codigo: resultadoAutenticar[0].codigo,
                                         nome: resultadoAutenticar[0].nome,
                                         senha: resultadoAutenticar[0].senha,
                                         aquarios: resultadoAquarios
@@ -35,7 +36,7 @@ function autenticar(req, res) {
                                 }
                             })
                     } else if (resultadoAutenticar.length == 0) {
-                        res.status(403).send("Email e/ou senha inválido(s)");
+                        res.status(403).send("codigo e/ou senha inválido(s)");
                     } else {
                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
                     }
@@ -54,15 +55,15 @@ function autenticar(req, res) {
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
+    var codigo = req.body.codigoServer;
     var senha = req.body.senhaServer;
     var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+    } else if (codigo == undefined) {
+        res.status(400).send("Seu codigo está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
     } else if (fkEmpresa == undefined) {
@@ -70,7 +71,7 @@ function cadastrar(req, res) {
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nome, codigo, senha, fkEmpresa)
             .then(
                 function (resultado) {
                     res.json(resultado);
